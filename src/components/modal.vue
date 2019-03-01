44927db61973a8b1f669d2e5856c097973b2347f
Pavel Dreit <pavel@dreit.ru>
2019-03-01 18:32:42 +0500

    Fix modal layouts

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -14,53 +14,18 @@ section.modal-auth
 								svg-icon(name='icon-arrow').to-left
 								| Нет личного кабинета? Зарегистрируйтесь
 				.block.register
-			form.right
+					.content
+						p.heavy-text Регистрация
+						p Заполните форму и создайте личный кабинет, чтобы вы могли записаться на курс. 
+						| Это займет не больше минуты.
+						hr
+						p Если он у вас уже есть, просто войдите.
+						a.transparent-button Войти в личный кабинет
+			form.left
 				.content
 					.close-btn
 
 					p.heavy-text Вход
-
-					app-input(
-						placeholder='e-mail'
-						data-vv-as='E-mail'
-						v-model.trim='email'
-						v-validate='"required|email"'
-						:error='errors.first("email")'
-						:class="{ 'error': errors.has('email'), 'success': fields.email && fields.email.valid}"
-						ref='asdasd'
-						name='email'
-						type='email')
-
-					app-input(
-						placeholder='Пароль'
-						data-vv-as='Пароль'
-						v-model.trim='password'
-						v-validate='"required"'
-						type='password'
-						:class="{ 'error': errors.has('password'), 'success': fields.password && fields.password.valid}"
-						:error='errors.first("password")'
-						name='password')
-
-					app-input(
-						placeholder='e-mail'
-						data-vv-as='E-mail'
-						v-model.trim='email'
-						v-validate='"required|email"'
-						:error='errors.first("email")'
-						:class="{ 'error': errors.has('email'), 'success': fields.email && fields.email.valid}"
-						ref='asdasd'
-						name='email'
-						type='email')
-
-					app-input(
-						placeholder='Пароль'
-						data-vv-as='Пароль'
-						v-model.trim='password'
-						v-validate='"required"'
-						type='password'
-						:class="{ 'error': errors.has('password'), 'success': fields.password && fields.password.valid}"
-						:error='errors.first("password")'
-						name='password')
 
 					app-input(
 						placeholder='e-mail'
@@ -98,6 +63,8 @@ section.modal-auth
 		},
 		data () {
 			return {
+				email: '',
+				password: ''
 			}
 		},
 	}
@@ -143,11 +110,11 @@ section.modal-auth
 		overflow: auto;
 		@media (max-width: 600px) {
 			display: block;
-			-webkit-overflow-scrolling:touch;
+			-webkit-overflow-scrolling: touch;
 		}
 	}
 	.modal-content {
-		height: 300px;
+		min-height: 300px;
 		max-width: 916px;
 		width: 100%;
 		position: relative;
@@ -163,26 +130,44 @@ section.modal-auth
 				margin: 13px 0;
 			}
 			p {
+				font-size: 15px;
 				margin: 0;
 				line-height: 1.5;
 			}
 			hr {
-				margin: 24px 0 20px;
+				margin: 22px 0 18px;
 				border: none;
 				border-radius: 1px;
 				height: 1px;
+				width: 100%;
+				min-height: 1px;
 				background-color: currentColor;
 				opacity: .8;
+			}
+			.transparent-button {
+				border: 2px solid rgba(255, 255, 255, .9);
+				color: white;
+				background-color: transparent;
+				padding: 10px 13px;
+				margin: 24px 0;
+				margin-bottom: 10px;
+				display: inline-block;
+				text-align: center;
+				font-size: 18px;
+				line-height: 1.33;
+				font-weight: bold;
+				font-family: var(--font-main);
 			}
 		}
 	}
 	.text-blocks {
 		margin: 20px var(--col-space);
-		height: 100%;
+		min-height: 100%;
 		display: flex;
 		z-index: -1;
 		position: relative;
 		overflow: hidden;
+		align-items: stretch;
 		@media (max-width: 600px) {
 			margin: 0;
 			flex-direction: column;
@@ -191,7 +176,7 @@ section.modal-auth
 			padding: * 10px;
 			width: 50%;
 			color: white;
-			height: calc(100% - 40px);
+			mih-height: calc(100% - 40px);
 			background: linear-gradient(to right, rgb(38, 70, 65) 25%, transparent 60%, rgb(97, 147, 139) 99%);
 			background-position: center center;
 			background-repeat: no-repeat;
@@ -227,6 +212,7 @@ section.modal-auth
 				height: calc(100% - 40px);
 				display: flex;
 				flex-direction: column;
+				align-items: flex-start;
 				@media (max-width: 600px) {
 					height: auto;
 					margin: 22px 10px;
@@ -243,6 +229,9 @@ section.modal-auth
 						font-family: var(--font-second);
 						line-height: 1.5;
 						.svg-icon {
+							@media (max-width: 600px) {
+								display: none;
+							}
 							height: 10px;
 							width: 20px;
 							display: inline-block;
@@ -286,7 +275,9 @@ section.modal-auth
 			margin: 30px auto;
 			max-width: calc(305px + 40px);
 			padding: * 20px;
-			padding-right: 40px;
+			@media (max-width: 600px) {
+				padding-right: 40px;
+			}
 		}
 		.app-input {
 			margin: 7px 0;
