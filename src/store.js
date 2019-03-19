9cf27cde1111add712aee2711c1ac69a64e7d2b6
Pavel Dreit <pavel@dreit.ru>
2019-03-19 15:43:26 +0500

    Add vuex-persistedstate

--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import createPersistedState from 'vuex-persistedstate'
+
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+	plugins: [createPersistedState({
+		paths: ['inputs'],
+	})],
 	state: {
 		appStates: {
 			isOpenedMenu: false,
