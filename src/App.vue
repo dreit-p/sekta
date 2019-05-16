984ff27007b2d5c06c922933804a7805a9782433
Pavel Dreit <pavel@dreit.ru>
2019-05-16 23:01:35 +0500

    Add mobile state defining

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,18 @@
 		}, false);
 	}(document, window));
 
+	function isMobile() {
+		if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	if (isMobile()) {
+		document.getElementsByTagName('body')[0].classList.add('mobile');
+	}
+
 	export default {
 		components: {
 			AppHeader: () => import('@/components/header.vue'),
