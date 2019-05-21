5f4e7cf4b2c67bb728fbe5268ffc882d5d0a9acf
Pavel Dreit <pavel@dreit.ru>
2019-05-21 19:44:29 +0500

    Fix style reassignment

--- src/views/personal/main.vue
+++ src/views/personal/main.vue
@@ -1,13 +1,5 @@
 <template lang="pug">
-div.personal-layout
-	header.main-header
-		.limit
-			router-link(class='logo', title='Sekta school logo', to='/personal')
-				svg-icon(name='logo-sekta')
-			.dropdown-menu
-				svg-icon(name='icon-login')
-				.text Анастасия Фадеева
-				.arrow-icon
+div
 	article.main-body
 		.limit
 			section.column
@@ -149,17 +141,7 @@ div.personal-layout
 	}
 </script>
 
-<style lang="postcss">
-	.personal-layout {
-		min-height: 100vh;
-		display: flex;
-		flex-direction: column;
-		justify-content: space-between;
-		.limit {
-			max-width: 1170px;
-		}
-	}
-
+<style lang="postcss" scoped>
 	.arrow-icon {
 		position: relative;
 		width: 20px;
@@ -190,77 +172,6 @@ div.personal-layout
 			transform: rotateZ(-45deg) rotateY(45deg);
 		}
 	}
-
-
-
-	header.main-header {
-		background-color: white;
-		padding: 28px *;
-		@media (max-width: 500px) {
-			padding: 10px * 6px;
-		}
-		.limit {
-			display: flex;
-			justify-content: space-between;
-		}
-		.logo {
-			display: block;
-			height: 54px;
-			width: 185px;
-			margin-right: 25px;
-			@media (max-width: 500px) {
-				height: 30px;
-				width: auto;
-				svg {
-					width: auto;
-				}
-			}
-		}
-		.dropdown-menu {
-			border-radius: 5px;
-			border: solid 1px #cbcbcb;
-			padding: 19px 20px 18px;
-			display: flex;
-			align-items: center;
-			cursor: pointer;
-			user-select: none;
-			@media (max-width: 500px) {
-				padding: 11px;
-			}
-			&:hover {
-				border-color: var(--accent_color);
-			}
-			.svg-icon--icon-login {
-				color: var(--accent_color);
-				stroke-width: 5px;
-				height: 14px;
-				width: 12px;
-				@media (max-width: 500px) {
-					height: 9px;
-				}
-			}
-			.text {
-				padding: * 13px;
-				font-size: 14px;
-				font-weight: bold;
-				white-space: nowrap;
-				@media (max-width: 500px) {
-					padding: * 5px;
-					font-size: 11px;
-				}
-			}
-			.arrow-icon {
-				position: relative;
-				width: 18px;
-				color: var(--accent_color);
-				@media (max-width: 500px) {
-					width: 15px;
-					margin-left: -5px;
-				}
-			}
-		}
-	}
-
 
 
 	article.main-body {
