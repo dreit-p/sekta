a3fbd7ab927725db86dd1bfbbbec2fe97388f8fe
Pavel Dreit <pavel@dreit.ru>
2019-05-22 15:54:04 +0500

    Add success-page

---
+++ src/views/success-message.vue
@@ -0,0 +1,110 @@
+<template lang="pug">
+section.not-found-page
+	.fullscreen-img
+		.background(:style="{ backgroundImage: `url('${require('@/assets/images/bg-success.jpg')}')`}")
+		.mobile-background(:style="{ backgroundImage: `url('${require('@/assets/images/bg-success-mobile.jpg')}')`}")
+		.content
+			.limit
+				.gray-text.huge Спасибо за заказ
+				.gray-text номер вашей заявки: 
+					span.green-text 123456789
+				.gray-text также мы выслали вам письмо с информацией 
+					br 
+					| о вашем заказе
+				router-link(tag='a', class='green-text', to='/') Вернуться на главную
+</template>
+
+<script>
+	export default {
+		name: 'SuccessMessage',
+		components: {
+		},
+		data () {
+			return {}
+		}
+	}
+</script>
+
+<style lang="postcss">
+.not-found-page {
+	.fullscreen-img {
+		min-height: 677px;
+		position: relative;
+		display: flex;
+		justify-content: stretch;
+		align-items: stretch;
+		align-content: stretch;
+		@media (max-width: 1024px) {
+			height: 100vw;
+			min-height: 500px;
+			max-height: 600px;
+		}
+		@media (max-width: 500px) {
+			height: auto;
+			max-height: none;
+		}
+	}
+	.background,
+	.mobile-background {
+		height: 100%;
+		width: 100%;
+		transition: background-position .1s, width .1s;
+		background-position: center;
+		background-size: cover;
+		background-repeat: no-repeat;
+		position: absolute;
+		top: 0;
+		right: 0;
+	}
+	.mobile-background {
+		display: none;
+		background-size: auto 110%;
+		background-position: 50% 100%;
+		@media (max-width: 500px) {
+			display: block;
+		}
+	}
+	.content {
+		padding: 10px *;
+		width: 100%;
+		flex-grow: 1;
+		.limit {
+			height: 100%;
+			width: 100%;
+			display: flex;
+			flex-direction: column;
+			align-items: center;
+		}
+		.gray-text {
+			text-shadow: -1.2px 2.8px 2px rgba(40, 38, 39, 0.4), -1.6px 3.7px 1px rgba(128, 128, 128, 0.16);
+			color: #c1c1c1;
+			font-size: 22px;
+			text-align: center;
+			letter-spacing: 1.1px;
+		}
+		.huge {
+			font-size: 60px;
+			text-align: center;
+			letter-spacing: 6px;
+			margin-right: -6px;
+			margin-top: 70px;
+			margin-bottom: 30px;
+			@media (max-width: 500px) {
+				margin-top: 0;
+			}
+		}
+		a {
+			text-decoration: underline;
+		}
+		.green-text {
+			text-shadow: 0px 4px 3.6px rgba(66, 66, 66, 0.45);
+			font-family: var(--font-second);
+			font-weight: bold;
+			font-size: 19px;
+			letter-spacing: 1.2px;
+			color: var(--accent_color);
+			margin: 60px *;
+		}
+	}
+}
+</style>
