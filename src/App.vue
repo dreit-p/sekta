53216e1e92cb55e9569578a831d3f0eec8ded2c9
Pavel Dreit <pavel@dreit.ru>
2019-05-22 14:59:28 +0500

    Remove scope for personal part & turn logs off

--- src/App.vue
+++ src/App.vue
@@ -48,11 +48,11 @@
 		},
 		created() {
 			// nothing defined here (when this.$route.path is other than "/")
-			console.log(this.$route, this.$route.meta.layout);
+			// console.log(this.$route, this.$route.meta.layout);
 		},
 		updated() {
 			// something defined here whatever the this.$route.path
-			console.log(this.$route, this.$route.meta.layout);
+			// console.log(this.$route, this.$route.meta.layout);
 		}
 	}
 </script>
