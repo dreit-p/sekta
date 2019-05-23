63526e08b9fa5e4948bd1167f0896fd0b0563b85
Pavel Dreit <pavel@dreit.ru>
2019-05-23 15:57:01 +0500

    Linting and little fixes

--- src/views/courses/pregnant.vue
+++ src/views/courses/pregnant.vue
@@ -43,18 +43,18 @@ div.course
 <script>
 	import { mapActions } from 'vuex'
 	export default {
-		name: 'for-mums',
+		name: 'Pregnants',
 		components: {
 			hero: () => import('@/components/heroes/aside-hero.vue'),
 			EntryForm: () => import('@/components/entry-form.vue'),
 			GreenBtn: () => import('@/components/form/green-btn.vue'),
 		},
+		data () {
+			return {}
+		},
 		methods: {
 			...mapActions(['setFormModalState'])
 		},
-		data () {
-			return {}
-		}
 	}
 </script>
 
--- src/views/courses/vip.vue
+++ src/views/courses/vip.vue
@@ -45,18 +45,18 @@ div.course
 <script>
 	import { mapActions } from 'vuex'
 	export default {
-		name: 'for-mums',
+		name: 'Vip',
 		components: {
 			hero: () => import('@/components/heroes/aside-hero.vue'),
 			EntryForm: () => import('@/components/entry-form.vue'),
 			GreenBtn: () => import('@/components/form/green-btn.vue'),
 		},
+		data () {
+			return {}
+		},
 		methods: {
 			...mapActions(['setFormModalState'])
 		},
-		data () {
-			return {}
-		}
 	}
 </script>
 
