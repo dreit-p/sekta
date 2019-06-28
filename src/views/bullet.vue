49a27c3e75bc8da3be7982449395fe5b51228501
Rustam <rustam.salmanov@immelman.ru>
2019-06-28 10:24:55 +0300

    correct bullet page

--- src/views/bullet.vue
+++ src/views/bullet.vue
@@ -1,55 +1,12 @@
 <template lang="pug">
 div.course
     vue-headful(
-            title="Мерч #sekta"
-            description="Футблоки, майки и толстовки #sekta. Размеры от xs до xl. Доставка по всей России."
-    )
-    hero(image='merch_bg-sektamerch.jpg', title='SEKTAMERCH', sideToRender='right')
-        p Купи крутую футболку #sektabootcampunited прямо сейчас! (Иначе они опять закончатся, и придется ждать следующего буткэмпа!) 
-        p Есть четыре размера: XS, S, M, L. <br>
-        p Подробности:
-        ul.merch__list 
-            li Ткань — хлопок 95/ лайкра 5, плотность 195;
-            li размер XS ширина 46/длина 59;
-            li размер S ширина 48/длина 61;
-            li размер М ширина 50/длина 64;
-            li размер L ширина 52/длина 66.  
-
-    section
-        entry-form(formType='merch')
-
-
-</template>
-
-<script>
-    import Vue from 'vue';
-    import vueHeadful from 'vue-headful';
-
-    Vue.component('vue-headful', vueHeadful);
-
-	export default {
-		name: 'SektaMerch',
-		components: {
-			hero: () => import('@/components/heroes/fullsize-hero.vue'),
-			CaptionSection: () => import('@/components/form/contents/caption-section.vue'),
-            GreenBtn: () => import('@/components/form/green-btn.vue'),
-			EntryForm: () => import('@/components/entry-form.vue'),
-        },
-		data () {
-			return {}
-		}
-	}
-</script>
-
-<template lang="pug">
-div.course
-    vue-headful(
             title="Bullet Journal от #sekta"
             description="Блокнот в точку - Bullet Journal от #sekta. Расчерченный календарь на 12 месяцев; трекеры привычек, настроения, сна и физической усталости; десятки страниц дневника для фантазии и жизни."
     )
     hero(image='bullet_bg-sektabullet.jpg', title='SEKTABULLET', sideToRender='right')
         p Буллет джорнал — это
-        ul.merch__list 
+        ul.merch__list
             li расчерченный календарь на 12 месяцев;
             li трекеры привычек, настроения, сна и физической усталости;
             li десятки страниц дневника для фантазии и жизни.
@@ -57,8 +14,6 @@ div.course
 
     section
         entry-form(formType='bullet')
-
-
 </template>
 
 <script>
@@ -68,7 +23,7 @@ div.course
     Vue.component('vue-headful', vueHeadful);
 
 	export default {
-		name: 'SektaMerch',
+		name: 'SektaBullet',
 		components: {
 			hero: () => import('@/components/heroes/fullsize-hero.vue'),
 			CaptionSection: () => import('@/components/form/contents/caption-section.vue'),
@@ -80,4 +35,3 @@ div.course
 		}
 	}
 </script>
-
