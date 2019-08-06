debb931765ebb47ce0896ee7523a15fcba8365f2
Ohiko <d.kolotushkina@metod.ru>
2019-08-06 10:48:07 +0300

    Products change offer, add pickup text

--- src/components/form/contents/inputs/product.vue
+++ src/components/form/contents/inputs/product.vue
@@ -119,15 +119,19 @@
 
 		app-checkbox(
 			name='termsAgree'
-			data-vv-as='обработка персональных данных'
+			data-vv-as='Согласие с доставкой, правилами возврата и публичной офертой'
 			v-model="termsAgree"
 			:required='true'
 			:class="{ 'error': myErrors.termsAgree, 'success': fields.termsAgree && fields.termsAgree.valid}"
 			:error='errors.first("termsAgree")'
 			@change="() => inputHandler('termsAgree')"
 		)
-			| Ознакомлен и согласен с условиями
-			a(href='#') обработки персональных данных
+			| Ознакомлен и согласен с условиями&nbsp;
+			a(href='https://sektaschool.ru/site/docs/delivery_merch.pdf', target='_BLANK', rel='noopener noreferrer') доставки,
+			|&nbsp;
+			a(href='https://sektaschool.ru/site/docs/return_policy_merch.pdf', target='_BLANK', rel='noopener noreferrer') правилами возврата товара
+			|  и
+			a(:href='"../docs/"+{1: "publicoffer_msk_new.pdf", 2: "publicoffer_spb_new.pdf", 3: "publicoffer_reg_new.pdf"}[userCity]' , target='_BLANK', rel='noopener noreferrer') публичной офертой
 
 		green-btn.green-btn_center(@click.prevent="makeOrder")
 			| Перейти к оплате
@@ -138,303 +142,316 @@
 =====================================*/
 
 let globalInputs = {
-	names: ['email', 'password', 'firstname', 'lastname'],
-	list: {}
+  names: ["email", "password", "firstname", "lastname"],
+  list: {}
 };
 
 for (var i = 0; i < globalInputs.names.length; i++) {
-	let name = globalInputs.names[i];
-	globalInputs.list[name] = {
-		get () {
-			return this.$store.state.inputs[name]
-		},
-		set (value) {
-			this.$store.commit('setInputData', {name: name, data: value})
-		}
-	};
+  let name = globalInputs.names[i];
+  globalInputs.list[name] = {
+    get() {
+      return this.$store.state.inputs[name];
+    },
+    set(value) {
+      this.$store.commit("setInputData", { name: name, data: value });
+    }
+  };
 }
 
 /*=====  End of define inputs  ======*/
-import api from '../../../../assets/api/index.js';
+import api from "../../../../assets/api/index.js";
 
 export default {
-	name: 'FormOnline',
-	components: {
-		AppInput: () => import('@/components/form/input.vue'),
-		AppCheckbox: () => import('@/components/form/checkbox.vue'),
-		AppDropdown: () => import('@/components/form/dropdown.vue'),
-		GreenBtn: () => import('@/components/form/green-btn.vue'),
-	},
-	computed: {
-		...globalInputs.list
-	},
-	data () {
-		return {
-			isMerch: false,
-			priceId: null,
-			prices: [],
-			quantity: null,
-			deliveryId: null,
-			deliveryOptions: [{id: 'pickup', name: 'Самовывоз из Санкт-Петербурга'}, {id: 'courier', name: 'Доставка курьером по России'}],
-			foxCities: [],
-			isCity: false,
-			city: {
-				city_name: '',
-				guid: ''
-			},
-			index: '',
-			address: '',
-			name: '',
-			phone: '',
-			promocode: '',
-			promo: {
-				amount: null
-			},
-			termsAgree: false,
-			hasError: false,
-			myErrors: {
-				priceId: null,
-				quantity: null,
-				deliveryId: null,
-				city: null,
-				index: null,
-				address: null,
-				name: null,
-				phone: null,
-				promocode: null,
-				termsAgree: null,
-			},
-			//Delivery
-			timerId: null,
-			isDeliveryCalc: false,
-			isDeliveryDone: false,
-			deliveryPrice: null,
-		}
-	},
-	created() {
-		this.isMerch = this.$route.name === 'sektamerch'
-		let product = this.isMerch ? 'TSHIRT' : 'BULLET'
-		api.getProduct(product).then(
-            res => {
-                this.prices = res.data.data.prices
-            },
-        )
-	},
-	methods: {
-		inputHandler(name) {
-			this.myErrors[name] = null
-			if (name === 'city' || name === 'address' || name === 'index') {
-				this.isDeliveryDone = false
-			}
-		},
-		pricesChangeHandler() {
-			if (this.promocode) {
-				this.checkPromocode(this.promocode)
-			}
-			this.myErrors.priceId = null
-		},
-		//Delivery
-		getCities(input) {
-			api.getFoxCities(input).then(
-				res => {
-					this.foxCities = res.data.data
-				}
-			)
-		},
-		selectCity(city) {
-			this.city = city
-			this.isCity = true
-		},
-		cityChangeHandler() {
-			setTimeout(this.hideCities, 150)
-		},
-		hideCities() {
-			this.foxCities = [];
-			clearTimeout(this.timerId)
-		},
-		cityInputHandler(input) {
-			if (this.timerId) {
-				clearTimeout(this.timerId)
-			}
-			this.inputHandler('city')
-			this.timerId = setTimeout(() =>this.getCities(input), 1000);
-			if (this.isCity) {
-				this.isCity = false
-				this.foxCities = [];
-				clearTimeout(this.timerId)
-			} else {
-				this.city.guid = ''
-			}
-		},
-		//Promocode
-		promoInputHandler() {
-			this.promo = {amount: null}
-			this.inputHandler('promocode')
-		},
-		promoChangeHandler(input) {
-			if (this.priceId) {
-				this.checkPromocode(input)
-			}
-		},
-		checkPromocode(code) {
-			api.reqDiscountedPrice(this.priceId, code).then(
-				res =>{
-					this.promo = res.data
-				},
-				rej => {
-					this.myErrors.promocode = [rej.response.data.message]
-				})
-		},
-		//Order
-		calcDeliveryPrice() {
-			this.checkDeliveryValid()
-			if (!this.hasError) {
-				this.isDeliveryDone = false
-				this.isDeliveryCalc = true
-				api.getFoxPrice(this.city.guid).then(
-					res => {
-						this.isDeliveryCalc = false
-						this.isDeliveryDone = true
-						this.deliveryPrice = res.data.data.price
-					}
-				)
-			}
-		},
-		checkDeliveryValid() {
-			this.hasError = false
-			if (!this.city.guid) {
-				this.myErrors.city = ['Выберите город из списка']
-				this.hasError = true
-			} else  {
-				this.checkInput('city')
-			}
-			this.checkInput('index')
-			this.checkInput('address')
-		},
-		checkOrderValid() {
-			this.hasError = false
-			if (this.deliveryId === 'courier') this.checkDeliveryValid()
+  name: "FormOnline",
+  components: {
+    AppInput: () => import("@/components/form/input.vue"),
+    AppCheckbox: () => import("@/components/form/checkbox.vue"),
+    AppDropdown: () => import("@/components/form/dropdown.vue"),
+    GreenBtn: () => import("@/components/form/green-btn.vue")
+  },
+  computed: {
+    ...globalInputs.list,
+    userCity() {
+      return this.$store.getters.getUserCity;
+    }
+  },
+  data() {
+    return {
+      isMerch: false,
+      priceId: null,
+      prices: [],
+      quantity: null,
+      deliveryId: null,
+      deliveryOptions: [
+        { id: "pickup", name: "Самовывоз из Санкт-Петербурга" },
+        { id: "courier", name: "Доставка курьером по России" }
+      ],
+      foxCities: [],
+      isCity: false,
+      city: {
+        city_name: "",
+        guid: ""
+      },
+      index: "",
+      address: "",
+      name: "",
+      phone: "",
+      promocode: "",
+      promo: {
+        amount: null
+      },
+      termsAgree: false,
+      hasError: false,
+      myErrors: {
+        priceId: null,
+        quantity: null,
+        deliveryId: null,
+        city: null,
+        index: null,
+        address: null,
+        name: null,
+        phone: null,
+        promocode: null,
+        termsAgree: null
+      },
+      //Delivery
+      timerId: null,
+      isDeliveryCalc: false,
+      isDeliveryDone: false,
+      deliveryPrice: null
+    };
+  },
+  created() {
+    this.isMerch = this.$route.name === "sektamerch";
+    let product = this.isMerch ? "TSHIRT" : "BULLET";
+    api.getProduct(product).then(res => {
+      this.prices = res.data.data.prices;
+    });
+  },
+  methods: {
+    inputHandler(name) {
+      this.myErrors[name] = null;
+      if (name === "city" || name === "address" || name === "index") {
+        this.isDeliveryDone = false;
+      }
+    },
+    pricesChangeHandler() {
+      if (this.promocode) {
+        this.checkPromocode(this.promocode);
+      }
+      this.myErrors.priceId = null;
+    },
+    //Delivery
+    getCities(input) {
+      api.getFoxCities(input).then(res => {
+        this.foxCities = res.data.data;
+      });
+    },
+    selectCity(city) {
+      this.city = city;
+      this.isCity = true;
+    },
+    cityChangeHandler() {
+      setTimeout(this.hideCities, 150);
+    },
+    hideCities() {
+      this.foxCities = [];
+      clearTimeout(this.timerId);
+    },
+    cityInputHandler(input) {
+      if (this.timerId) {
+        clearTimeout(this.timerId);
+      }
+      this.inputHandler("city");
+      this.timerId = setTimeout(() => this.getCities(input), 1000);
+      if (this.isCity) {
+        this.isCity = false;
+        this.foxCities = [];
+        clearTimeout(this.timerId);
+      } else {
+        this.city.guid = "";
+      }
+    },
+    //Promocode
+    promoInputHandler() {
+      this.promo = { amount: null };
+      this.inputHandler("promocode");
+    },
+    promoChangeHandler(input) {
+      if (this.priceId) {
+        this.checkPromocode(input);
+      }
+    },
+    checkPromocode(code) {
+      api.reqDiscountedPrice(this.priceId, code).then(
+        res => {
+          this.promo = res.data;
+        },
+        rej => {
+          this.myErrors.promocode = [rej.response.data.message];
+        }
+      );
+    },
+    //Order
+    calcDeliveryPrice() {
+      this.checkDeliveryValid();
+      if (!this.hasError) {
+        this.isDeliveryDone = false;
+        this.isDeliveryCalc = true;
+        api.getFoxPrice(this.city.guid).then(res => {
+          this.isDeliveryCalc = false;
+          this.isDeliveryDone = true;
+          this.deliveryPrice = res.data.data.price;
+        });
+      }
+    },
+    checkDeliveryValid() {
+      this.hasError = false;
+      if (!this.city.guid) {
+        this.myErrors.city = ["Выберите город из списка"];
+        this.hasError = true;
+      } else {
+        this.checkInput("city");
+      }
+      this.checkInput("index");
+      this.checkInput("address");
+    },
+    checkOrderValid() {
+      this.hasError = false;
+      if (this.deliveryId === "courier") this.checkDeliveryValid();
 
-			this.checkInput('priceId')
-			if (this.isMerch) {
-				this.checkInput('quantity')
-				if (this.quantity > (this.priceId ? this.prices.find(p => p.id === this.priceId).available_quantity : 0)) {
-					this.hasError = true
-				}
-			}
-			this.checkInput('deliveryId')
-			this.checkInput('name')
-			this.checkInput('phone')
-			this.checkInput('termsAgree')
-		},
-		checkInput(name) {
-			if (!this[name]) {
-				this.myErrors[name] = ['']
-				this.hasError = true
-			}
-		},
-		makeOrder() {
-			this.checkOrderValid()
-			if (this.hasError) return
-			let order = {
-				price_id: this.priceId,
-				delivery_type: this.deliveryId,
-				recipient_name: this.name,
-				recipient_phone: this.phone,
-			}
-			if (this.deliveryId === 'courier') {
-				order.delivery_city = this.city.city_name
-				order.delivery_index = this.index
-				order.delivery_address = this.address
-			}
-			if (this.isDeliveryDone) order.delivery_price = this.deliveryPrice
-			api.makeProguctOrder(order, this.$store.state.user.token).then(res => {
-				this.makePayment(res.data.order_id)
-			}, rej => {
-				this.errorHandler(rej.response.data.errors)
-			})
-		},
-		makePayment(order_id) {
-			let data = {
-				order_id,
-				price_id: this.priceId
-			}
-			if (this.promo.amount) data.promocode = this.promo.promocode_code
-			data.quantity = this.quantity ? this.quantity : 1
-			api.makeProguctPayment(data, this.$store.state.user.token).then(res => {
-			}, rej => {
-			})
-		},
-		errorHandler(errors) {
-			for (let key in errors) {
-				let newKey = key.replace('delivery_', '').replace('recipient_', '')
-				this.myErrors[newKey] = errors[key]
-			}
-		}
-	}
-}
+      this.checkInput("priceId");
+      if (this.isMerch) {
+        this.checkInput("quantity");
+        if (
+          this.quantity >
+          (this.priceId
+            ? this.prices.find(p => p.id === this.priceId).available_quantity
+            : 0)
+        ) {
+          this.hasError = true;
+        }
+      }
+      this.checkInput("deliveryId");
+      this.checkInput("name");
+      this.checkInput("phone");
+      this.checkInput("termsAgree");
+    },
+    checkInput(name) {
+      if (!this[name]) {
+        this.myErrors[name] = [""];
+        this.hasError = true;
+      }
+    },
+    makeOrder() {
+      this.checkOrderValid();
+      if (this.hasError) return;
+      let order = {
+        price_id: this.priceId,
+        delivery_type: this.deliveryId,
+        recipient_name: this.name,
+        recipient_phone: this.phone
+      };
+      if (this.deliveryId === "courier") {
+        order.delivery_city = this.city.city_name;
+        order.delivery_index = this.index;
+        order.delivery_address = this.address;
+      }
+      if (this.isDeliveryDone) order.delivery_price = this.deliveryPrice;
+      api.makeProguctOrder(order, this.$store.state.user.token).then(
+        res => {
+          this.makePayment(res.data.order_id);
+        },
+        rej => {
+          this.errorHandler(rej.response.data.errors);
+        }
+      );
+    },
+    makePayment(order_id) {
+      let data = {
+        order_id,
+        price_id: this.priceId
+      };
+      if (this.promo.amount) data.promocode = this.promo.promocode_code;
+      data.quantity = this.quantity ? this.quantity : 1;
+      api
+        .makeProguctPayment(data, this.$store.state.user.token)
+        .then(res => {}, rej => {});
+    },
+    errorHandler(errors) {
+      for (let key in errors) {
+        let newKey = key.replace("delivery_", "").replace("recipient_", "");
+        this.myErrors[newKey] = errors[key];
+      }
+    }
+  }
+};
 </script>
 
 <style lang="postcss">
-	.content.register {
-		max-width: calc(335px + 15px * 2) !important;
-	}
-	.merch-title {
-		margin-top: 12px;
-	}
-	.green-btn_center.green-btn {
-		margin-left: auto;
-		margin-right: auto;
-	}
-	.merch__total-price {
-		margin: 15px 0;
-		font-weight: 900;
-		font-family: 'Montserrat', 'Uni Sans', 'Tahoma', 'Segoe UI', arial, sans-serif;
-	}
-	.delivery-city {
-		position: relative;
-	}
-	.fox-cities{
-		&__list {
-			position: absolute;
-			top: 46px;
-			left: 0;
-			background-color: white;
-			z-index: 3;
-			width: 100%;
-			max-height: 609px;
-			overflow: auto;
-		}
-		&__item {
-			display: flex;
-			align-items: center;
-			width: 100%;
-			min-height: 30px;
-			padding-left: 10px;
-			border: 1px solid #808080;
-			border-top: none;
-			&:hover {
-				color: white;
-				background-color: blue;
-			}
-		}
-	}
-	.delivery-block {
-		font-size: 14px;
-		font-family: 'Open Sans', 'PT Sans', 'Segoe UI', arial, sans-serif;
-	}
-	.delivery-calculating {
-		margin: 15px 0;
-	  	font-style: italic;
-	}
-	.delivery-price span {
-		font-weight: bold;
-		font-size: 16px;
-	}
-	.delivery-info {
-		margin-top: 5px;
-		margin-bottom: 10px;
-		font-size: 12px;
-		color: #808080;
-	}
+.content.register {
+  max-width: calc(335px + 15px * 2) !important;
+}
+.merch-title {
+  margin-top: 12px;
+}
+.green-btn_center.green-btn {
+  margin-left: auto;
+  margin-right: auto;
+}
+.merch__total-price {
+  margin: 15px 0;
+  font-weight: 900;
+  font-family: "Montserrat", "Uni Sans", "Tahoma", "Segoe UI", arial, sans-serif;
+}
+.delivery-city {
+  position: relative;
+}
+.fox-cities {
+  &__list {
+    position: absolute;
+    top: 46px;
+    left: 0;
+    background-color: white;
+    z-index: 3;
+    width: 100%;
+    max-height: 609px;
+    overflow: auto;
+  }
+  &__item {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    min-height: 30px;
+    padding-left: 10px;
+    border: 1px solid #808080;
+    border-top: none;
+    &:hover {
+      color: white;
+      background-color: blue;
+    }
+  }
+}
+.delivery-block {
+  font-size: 14px;
+  font-family: "Open Sans", "PT Sans", "Segoe UI", arial, sans-serif;
+}
+.delivery-calculating {
+  margin: 15px 0;
+  font-style: italic;
+}
+.delivery-price span {
+  font-weight: bold;
+  font-size: 16px;
+}
+.delivery-info {
+  margin-top: 5px;
+  margin-bottom: 10px;
+  font-size: 12px;
+  color: #808080;
+}
+.app-dropdown.success .caption {
+  display: block !important;
+  color: #808080 !important;
+}
 </style>
