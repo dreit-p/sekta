7ed98feb3ebae150a6185d9b42e5868d4ef14278
Kirill Matasov <kirill.matasov@immelman.ru>
2019-08-28 12:25:51 +0300

    Make redirect to account when logged on special page

--- src/views/personal/account-login.vue
+++ src/views/personal/account-login.vue
@@ -1,14 +1,24 @@
-<template>
-    <div>Redirect...</div>
+<template lang="pug">
+	div
+		p Redirect
+		form(ref="form" :action="url" method="post")
+			input(type="hidden" name="token" :value="token")
+
 </template>
+
 <script>
+
 export default {
-	data() {
-		return 
+	computed: {
+		url() {
+			return process.env.VUE_APP_ACCOUNT_URL + '/login.php'
+		},
+		token() {
+			return this.$store.getters.authToken
+		},
 	},
-	created() {
-		//TODO 
-		console.log(process.env.VUE_APP_ACCOUNT_URL)
-	}
+	mounted() {
+		this.$refs.form.submit()
+	},
 }
 </script>
