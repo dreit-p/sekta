efc8cdfdbd29cc4c938a60f2510ebb44913351ed
Pavel Dreit <dreit.p@gmail.com>
2019-08-29 22:54:59 +0500

    Improve webp

--- src/components/webp-img.vue
+++ src/components/webp-img.vue
@@ -1,19 +1,23 @@
 <template>
 	<picture>
-		<source type="image/webp" :srcset="fileName[0]">
-		<source type="image/jpeg" :srcset="fileName[1]">
-		<img :src="fileName[1]" :alt="alt">
+		<source :type="'image/'+files[0].type" :srcset="files[0].link">
+		<source :type="'image/'+files[1].type" :srcset="files[1].link">
+		<img :src="files[1].link" :alt="alt" v-bind="$attrs" :style='imgStyle'>
 	</picture>
 </template>
 
 <script>
 export default {
 	name: 'WebpImg',
-
+	inheritAttrs: false,
 	props: {
 		src: {
 			type: String,
 			required: true
+		},
+		imgStyle: {
+			type: Object,
+			default: ()=>{},
 		},
 		alt: {
 			type: String,
@@ -21,14 +25,24 @@ export default {
 		},
 	},
 	computed: {
-		fileName () {
+		files () {
 			let location = 'assets/images/';
-			let fileName = this.src.match(/([\w,\s-]+)\.[A-Za-z]{3}/)[1];
+			let fileName = this.src.match(/([\w,\s-\/]+)\.[A-Za-z]{3}/)[1];
 			let fileExt = this.src.slice((this.src.lastIndexOf(".") - 1 >>> 0) + 2);
 
 			let file = require(`@/${location}${fileName}.${fileExt}`);
+
 			let extless =  file.replace(/\.[^.]+$/, "");
-			return [extless + '.webp', extless + '.jpg']
+			return [
+			{
+				type: 'webp',
+				link: extless + '.webp',
+			},
+			{
+				type: fileExt,
+				link: `${extless}.${fileExt}`,
+			}
+			]
 		},
 
 	}
