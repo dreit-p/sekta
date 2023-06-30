220f5f3ddf86dff589c3ee9b2813df530fadc4f0
Pavel Dreit <pavel@dreit.ru>
2023-06-30 15:55:09 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -13,6 +13,8 @@ Vue.use(AsyncComputed)
 =            Validator            =
 =================================*/
 
+const test = {"123":"123"}
+
 import VeeValidate, { Validator } from 'vee-validate';
 import ru from 'vee-validate/dist/locale/ru.js';
 
